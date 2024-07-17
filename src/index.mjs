72b7a5952cc444a632c24f5355b2169c13a1e7bd
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-07-18 02:14:36 +0500

    Deployment Changes

--- src/index.mjs
+++ src/index.mjs
@@ -17,17 +17,21 @@ dotenv.config();
 const dbUrl = process.env.DATABASE_URL;
 const aUrl = process.env.O_URL;
 const PORT = process.env.PORT || 3000;
-const test = process.env.APII
+const test = process.env.APII;
+const password = process.env.PASSWORD;
+const appname = process.env.APPNAME;
 
 app.use(
   cors({
-    origin:`http://localhost:${5173}`,
+    origin: `http://localhost:${5173}`,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   })
 );
 mongoose
-  .connect(`mongodb://localhost:27017/ecommerce`)
+  .connect(
+    `mongodb+srv://abdul:${password}@multivendorecommerce.ysfgkez.mongodb.net/?retryWrites=true&w=majority&appName=MultiVendorECommerce`
+  )
   .then(() => console.log("Connected to database"));
 app.use(
   session({
