853efd6a314f141cbb97dfd9d5f2bf1b0a89d866
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-07-18 18:10:34 +0500

    Cloud Db

--- src/index.mjs
+++ src/index.mjs
@@ -10,28 +10,21 @@ import productRouter from "./Routes/products/products.mjs";
 import ordersRouter from "./Routes/orders/orders.mjs";
 import dealRouter from "./Routes/deals/deals.mjs";
 import dotenv from "dotenv";
-
-const app = express();
 dotenv.config();
 
-const dbUrl = process.env.DATABASE_URL;
-const aUrl = process.env.O_URL;
-const PORT = process.env.PORT || 3000;
-const test = process.env.APII;
-const password = process.env.PASSWORD;
-const appname = process.env.APPNAME;
+const app = express();
+const PORT = process.env.PORT ;
+const mongodburi = process.env.MONGODB_CONNECTION_STRING
 
 app.use(
   cors({
-    origin: `http://localhost:${5173}`,
+    origin: `http://localhost:5173`,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   })
 );
 mongoose
-  .connect(
-    `mongodb+srv://abdul:${password}@multivendorecommerce.ysfgkez.mongodb.net/?retryWrites=true&w=majority&appName=MultiVendorECommerce`
-  )
+  .connect(mongodburi)
   .then(() => console.log("Connected to database"));
 app.use(
   session({
@@ -59,5 +52,4 @@ app.use(dealRouter);
 
 app.listen(PORT, () => {
   console.log(`Ruuning On Port ${PORT}`);
-  console.log(test);
 });
