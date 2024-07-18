4e8a4ba7dca3de52b477d27ad756c6512dfd95dc
AbdulHaseebAhmad <79489830+AbdulHaseebAhmad@users.noreply.github.com>
2024-07-18 19:39:07 +0500

    CORS alt

--- src/index.mjs
+++ src/index.mjs
@@ -18,7 +18,7 @@ const mongodburi = process.env.MONGODB_CONNECTION_STRING
 
 app.use(
   cors({
-    origin: 'https://multivendorecommerce-00.web.app',
+    origin: ['https://multivendorecommerce-00.web.app','https://multivendorecommerce-00.web.app/', 'http://localhost:5173'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   })
